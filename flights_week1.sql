5c3b7a9d2bc7422b9e916590f7158fb5def15a25
Stephanie Roark <steph.roark@gmail.com>
2018-07-15 15:46:16 -0700

    week 1 submission update

--- flights_week1.sql
+++ flights_week1.sql
@@ -1,7 +1,7 @@
 
 # Homework 1 Select and Aggregation
 
-# Question 1 
+# Question 1 Which destination in the flights database is the furthest distance away, based on information in the flights table?
 
 SELECT origin, dest, distance 
 FROM flights 
@@ -12,25 +12,25 @@ ORDER BY distance DESC LIMIT 1;
 SELECT MAX(distance) 
 FROM flights;
 
-# Question 2 , Part 1
+# Question 2 , Part 1 What are the different numbers of engines in the planes table? 
 
 SELECT engines 
 FROM planes 
 GROUP BY engines ASC;
 
-# Question 2, Part 2
+# Question 2, Part 2 For each number of engines, which aircraft have the most number of seats? 
 
 SELECT type, engines, MAX(seats) 
 FROM planes 
 GROUP BY type,engines;
 
-# Question 3
+# Question 3 	Show the total number of flights. 
 
 SELECT COUNT(*) 
 FROM flights 
 WHERE air_time > 0;
 
-# Question 4
+# Question 4	Show the total number of flights by airline (carrier). 
 
 SELECT COUNT(*), carrier AS flights 
 FROM flights 
@@ -39,7 +39,7 @@ GROUP BY carrier
 ORDER BY COUNT(*) ASC;
 
 
-# Question 5
+# Question 5	Show all of the airlines, ordered by number of flights in descending order. 
 
 SELECT carrier,COUNT(*) AS flights 
 FROM flights 
@@ -47,7 +47,7 @@ WHERE air_time  is not null
 GROUP BY carrier 
 ORDER BY flights DESC;
 
-# Question 6
+# Question 6	Show only the top 5 airlines, by number of flights, ordered by number of flights in descending order. 
 
 SELECT carrier, COUNT(*) AS flights 
 FROM flights 
@@ -55,7 +55,7 @@ WHERE air_time  is not null
 GROUP BY carrier 
 ORDER BY flights DESC LIMIT 5;
  
- # Question 7
+ # Question 7.	Show only the top 5 airlines, by number of flights of distance 1,000 miles or greater, ordered by number of flights in descending order. 
 
 SELECT carrier, COUNT(*) AS flights 
 FROM flights 
@@ -63,7 +63,7 @@ WHERE air_time  is not null AND distance >= 1000
 GROUP BY carrier 
 ORDER BY flights DESC LIMIT 5;
 
-# Question 8, Part 1
+# Question 8, Part 1: What is the total number of flight miles per airline? 
 
 SELECT carrier, SUM(distance) AS miles 
 FROM flights 
@@ -71,7 +71,7 @@ WHERE air_time is not null
 GROUP BY carrier 
 ORDER BY miles DESC;
 
-# Question 8, Part 2
+# Question 8, Part 2: What are the average flight miles by carrier in the year 2013?
 
 SELECT carrier, SUM(distance) AS miles 
 FROM flights 
